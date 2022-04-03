f483c998d50d3bff35f61356b612921298ee7ff0
VinceCai <vioczai@outlook.com>
2022-04-03 02:40:32 -0400

    Added Colour support to Network

--- COMP397-W2022-Lab11/Assets/[Scripts]/NetworkPlayerBehaviour.cs
+++ COMP397-W2022-Lab11/Assets/[Scripts]/NetworkPlayerBehaviour.cs
@@ -6,17 +6,30 @@ using UnityEngine;
 public class NetworkPlayerBehaviour : NetworkBehaviour
 {
     public float speed;
+    public MeshRenderer meshrenderer;
 
     private NetworkVariable<float> verticalPosition = new NetworkVariable<float>();
     private NetworkVariable<float> horizontalPosition = new NetworkVariable<float>();
 
+    private NetworkVariable<Color> materialColor = new NetworkVariable<Color>(); 
+
     private float localHorizontal;
     private float localVertical;
+    private Color localColor;
 
-    // Start is called before the first frame update
-    void Start()
+
+	void Awake()
+	{
+        materialColor.OnValueChanged += ColorOnChange;
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
+        meshrenderer = GetComponent<MeshRenderer>();
         RandomSpawnPosition();
+
+        meshrenderer.material.SetColor("_Color", materialColor.Value);
     }
 
     // Update is called once per frame
@@ -36,10 +49,22 @@ public class NetworkPlayerBehaviour : NetworkBehaviour
     void ServerUpdate()
 	{
         transform.position = new Vector3(transform.position.x + horizontalPosition.Value, transform.position.y, transform.position.z + verticalPosition.Value);
-	}
+	
+        if (meshrenderer.material.GetColor("_Color") != materialColor.Value)
+		{
+            meshrenderer.material.SetColor("_Color", materialColor.Value);
+		}
+        
+    }
 
     public void RandomSpawnPosition()
     {
+        var r = Random.Range(0, 1.0f);
+        var g = Random.Range(0, 1.0f);
+        var b = Random.Range(0, 1.0f);
+        var color = new Color(r, g, b);
+        localColor = color;
+
         var x = Random.Range(-10.0f, 10.0f);
         var z = Random.Range(-10.0f, 10.0f);
         transform.position = new Vector3(x, 1.0f, z);
@@ -60,6 +85,11 @@ public class NetworkPlayerBehaviour : NetworkBehaviour
 
             UpdateClientPositionServerRpc(horizontal, vertical);
         }
+
+        if (localColor != materialColor.Value)
+		{
+            SetClientColorServerRpc(localColor);
+		}
     }
 
     [ServerRpc]
@@ -67,5 +97,18 @@ public class NetworkPlayerBehaviour : NetworkBehaviour
 	{
         horizontalPosition.Value = horizontal;
         verticalPosition.Value = vertical;
+	}
+
+    [ServerRpc]
+    public void SetClientColorServerRpc(Color color)
+	{
+        materialColor.Value = color;
+
+        meshrenderer.material.SetColor("_Color", materialColor.Value);
+    }
+
+    void ColorOnChange(Color oldColor, Color newColor)
+	{
+        GetComponent<MeshRenderer>().material.color = materialColor.Value;
 	}
 }
